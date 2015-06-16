ccf8cf1adfa8706e3c782f6707a20adece5865e7
Sebastian Lohmeier <sl@monochromata.de>
2015-06-16 09:32:16 +0200

    + adding a first failing test case

---
+++ src/test/java/rit/eyeTracking/AbstractEventTest.java
@@ -0,0 +1,59 @@
+package rit.eyeTracking;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class AbstractEventTest {
+
+	@Test
+	public void testHashCode() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testAbstractEvent() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetID() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testAddAttribute() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetAttribute() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetAttributes() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testClear() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testToString() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetSerializable() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testEqualsObject() {
+		fail("Not yet implemented");
+	}
+
+}
